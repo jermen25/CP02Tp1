c6240971d618ba1b7aa94d20bdca5f7f37bc1af6
Ménétrier Jérôme <menetrier.jerome@hotmail.com>
2020-12-04 16:20:19 +0100

    Ajoutez des fichiers projet.

---
+++ CP02Tp1/FrmListe.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace CP02Tp1
+{
+    partial class FrmListe
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstListe = new System.Windows.Forms.ListBox();
+            this.txtSaisie = new System.Windows.Forms.TextBox();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstListe
+            // 
+            this.lstListe.FormattingEnabled = true;
+            this.lstListe.ItemHeight = 20;
+            this.lstListe.Location = new System.Drawing.Point(61, 53);
+            this.lstListe.Name = "lstListe";
+            this.lstListe.Size = new System.Drawing.Size(176, 44);
+            this.lstListe.TabIndex = 0;
+            // 
+            // txtSaisie
+            // 
+            this.txtSaisie.Location = new System.Drawing.Point(61, 154);
+            this.txtSaisie.Name = "txtSaisie";
+            this.txtSaisie.Size = new System.Drawing.Size(176, 27);
+            this.txtSaisie.TabIndex = 0;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(32, 264);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(94, 29);
+            this.btnAjouter.TabIndex = 2;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(178, 264);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(94, 29);
+            this.btnSupprimer.TabIndex = 3;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // FrmListe
+            // 
+            this.AcceptButton = this.btnAjouter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 416);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.txtSaisie);
+            this.Controls.Add(this.lstListe);
+            this.Name = "FrmListe";
+            this.Text = "Ma Super List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstListe;
+        private System.Windows.Forms.TextBox txtSaisie;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnSupprimer;
+    }
+}
+
---
+++ CP02Tp1/FrmListe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CP02Tp1
+{
+    public partial class FrmListe : Form
+    {
+        public FrmListe()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            if (txtSaisie.Text == "" ) // Si aucun item sélectionné
+            {
+                // Message d'avertissement – attention instruction sur une seule ligne
+                MessageBox.Show("Vous devez remplir le champs", "Message d'erreur", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                // Ajouter le contenu de la textbox à la liste
+                lstListe.Items.Add(txtSaisie.Text);
+                // Effacer le contenu de la textbox
+                txtSaisie.Text = "";
+                // txtSaisie.Clear() --> aurait la même fonction
+            }
+            // Remettre le focus (le curseur) sur la textbox
+            txtSaisie.Focus();
+        }
+
+       
+            private void btnSupprimer_Click(object sender, EventArgs e)
+            {
+            if (lstListe.SelectedIndex == -1) // Si aucun item sélectionné
+            {
+                // Message d'avertissement – attention instruction sur une seule ligne
+                MessageBox.Show("Vous devez sélectionner un élément dans la liste", "Message d'erreur", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                // Suppression possible et effectuée
+                lstListe.Items.RemoveAt(lstListe.SelectedIndex);
+            }
+        }
+
+        
+    }
+}
